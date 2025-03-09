2323e4dd893dc738dad2e100454056ba4535f99f
Phanindra-Nelavalli <nelavalliphanindra4@gmail.com>
2025-03-09 18:38:57 +0530

    FAQs

---
+++ FAQs.js
@@ -0,0 +1,24 @@
+export const faqs = [
+    {
+      question: "What is VitaCraft?",
+      answer: "VitaCraft is a powerful online resume builder that helps you create professional, ATS-friendly resumes quickly and easily using customizable templates.",
+    },
+    {
+      question: "Is VitaCraft free to use?",
+      answer: "Yes! VitaCraft offers free resume creation and downloads, with premium options available for advanced templates and customization.",
+    },
+    {
+      question: "Can I edit my resume after downloading it?",
+      answer: "Yes! If you download your resume in DOCX format, you can edit it anytime. However, PDFs are best for final versions to keep formatting intact.",
+    },
+    {
+      question: "Does VitaCraft offer job-specific resume templates?",
+      answer: "Absolutely! VitaCraft provides industry-specific templates to help you craft a resume tailored to your desired job.",
+    },
+    {
+      question: "How does the Job Match Score feature work?",
+      answer: "VitaCraft’s Job Match Score analyzes your resume against job descriptions to check how well your resume aligns with the job requirements.",
+    },
+  ];
+
+  export default faqs;
