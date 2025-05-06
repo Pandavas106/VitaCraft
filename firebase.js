44e60698a22f73be69f6194751ffaeae59d0e2a0
LINGALA RAJESH <136982328+Rajesh2607@users.noreply.github.com>
2025-05-06 14:46:24 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,5 +1,9 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+import { GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
 
 const firebaseConfig = {
   apiKey: "AIzaSyDyUAZq-gVJaZLrL9-y6FeCBXx-GjLk6U8",
@@ -8,8 +12,13 @@ const firebaseConfig = {
   storageBucket: "vitacraft-70fcc.firebasestorage.app",
   messagingSenderId: "427960339262",
   appId: "1:427960339262:web:0fae26c57e6b9e39337a6f",
-  measurementId: "G-NHWGF5YFBE"
+  measurementId: "G-NHWGF5YFBE",
 };
 
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+const auth = getAuth(app);
+const db = getFirestore(app);
+const provider = new GoogleAuthProvider();
+
+export { analytics, auth, db, provider };
